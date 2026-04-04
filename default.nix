aa47a74109ba00d428a0aaec73573db095c2bbf9
Luytan <luytan@khora.me>
2026-04-04 23:53:40 +0200

    Format

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation (finalAttrs: {
   version = "1.0.0";
 
   src = ./wallpapers;
-  
+
   installPhase = ''
     mkdir -p $out/wallpapers
     cp ./* $out/wallpapers/
@@ -17,7 +17,7 @@ stdenv.mkDerivation (finalAttrs: {
     description = "My wallpapers";
     homepage = "";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ 
+    maintainers = with lib.maintainers; [
       "luytan@khora.me"
     ];
   };
--- flake.nix
+++ flake.nix
@@ -3,16 +3,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, ... }@inputs: let
-    forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
-  in {
-    packages = forAllSys (system: let
-      pkgs = import nixpkgs {inherit system; };
-      my_wallpapers = pkgs.callPackage ./. {
-        inherit (inputs);
-      };
-    in {
-      default = my_wallpapers;
-    });
-  };
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
+    in
+    {
+      packages = forAllSys (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          my_wallpapers = pkgs.callPackage ./. {
+            inherit (inputs) ;
+          };
+        in
+        {
+          default = my_wallpapers;
+        }
+      );
+    };
 }
