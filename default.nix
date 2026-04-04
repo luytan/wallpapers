3739b7fe58d0e6dc7c405d37957ca149df0a1c80
Luytan <luytan@khora.me>
2026-04-04 23:52:48 +0200

    Somehow managed to make a working package

---
+++ default.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  stdenv,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "my_wallpapers";
+  version = "1.0.0";
+
+  src = ./wallpapers;
+  
+  installPhase = ''
+    mkdir -p $out/wallpapers
+    cp ./* $out/wallpapers/
+  '';
+
+  meta = {
+    description = "My wallpapers";
+    homepage = "";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ 
+      "luytan@khora.me"
+    ];
+  };
+})
